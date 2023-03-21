a43a07b48644a1e7da7e24234f1d1d4973917785
Daiki Anjiki <daikianjiki@gmail.com>
2023-03-20 21:18:52 -0700

    edited user to add username/password

--- bank-app/src/app/model/user.ts
+++ bank-app/src/app/model/user.ts
@@ -1,8 +1,10 @@
 export interface User {
     id: number,
-    firstName: string,
-    lastName: string, 
-    address: string,
-    phone: number,
-    email: string
+    username: string, //user will use this to log in.
+    password: string, //user will use this to log in.
+    firstName: string, //user's name - won't need it for anything else. (May be in the main page? i.e. hello, {{User.firstname}})
+    lastName: string, //user's last name - won't need it for anything else.
+    address: string, //user's address - won't need it for anything else
+    phone: number, //user's phone number - won't need it for anything else. (Maybe two-way authenticator?)
+    email: string //user's email - we can send an email to the user to verify account creation?
 }
