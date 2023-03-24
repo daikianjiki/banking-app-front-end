62ce3021c9c6507a43dd5634f1f04d913fba1695
Daiki Anjiki <daikianjiki@gmail.com>
2023-03-24 13:05:31 -0700

    minor changes, user functional

--- bank-app/src/app/components/update-user/update-user.component.ts
+++ bank-app/src/app/components/update-user/update-user.component.ts
@@ -22,24 +22,18 @@ export class UpdateUserComponent {
   show : boolean = false;
 
   constructor(private userService : UserService) {}
-
+  
   saveChanges() : void {
-<<<<<<< HEAD
-    this.userService.patchUser(this.user, this.user.userId).subscribe(json => {
-=======
-    if (this.user.id == undefined){
+    if (this.user.userId == undefined){
       throw new Error("update-user.saveChages(): user ID is undefined");
     }
-
-    this.userService.patchUser(this.user, this.user.id).subscribe(json => {
->>>>>>> d2ccae20b16a55e04670db5f14fdd5ecc0ed49fd
+    this.userService.patchUser(this.user, this.user.userId).subscribe(json => {
       this.user = json;
       console.log("updated user: " +this.user)});
       this.openUpdate();
   }
-
   //this will show the update menu when clicked. When you saveChanges(), it will unmount.
-  openUpdate() {
+  openUpdate() : void {
     this.show = !this.show;
   }
 }
