c4c886427663c45c3f2d7b8748cc9ed28729dc5c
coder <79178809+c0deblack@users.noreply.github.com>
2023-03-27 17:02:17 -0400

    Reconfiguring transactions for updated backend

--- bank-app/src/app/model/account.ts
+++ bank-app/src/app/model/account.ts
@@ -1,5 +1,5 @@
 export interface Account {
-    id: number, //is this the account number?
-    type: string, //checking, savings etc.
-    balance: number, //this will match the balance in transaction
+    accountId?: number, //is this the account number?
+    type?: string, //checking, savings etc.
+    balance?: number, //this will match the balance in transaction
 }
--- bank-app/src/app/model/transaction.ts
+++ bank-app/src/app/model/transaction.ts
@@ -1,8 +1,12 @@
+import { Account } from "./account";
+
 export interface Transaction {
-    transactionId: number,
-    timestamp: number, //date of the transaction i.e 'March 20, 2023' -- is there Data datatype?
-    description: string, //description of the transaction i.e 'Transfer to Savings Account'
+    transactionId?: number,
+    timestamp?: number, //date of the transaction i.e 'March 20, 2023' -- is there Data datatype?
+    description?: string, //description of the transaction i.e 'Transfer to Savings Account'
     transactionType: string, //type of the transaction i.e withdrawal, deposit etc.
     amount: number, //amount of transaction. i.e. -$100.00 for withdrawal or transfer.
-    balance: number //total balance after the transaction. i.e pre-balance: $500.00, transfer $100.00, balance = $400.00
+    balance?: number, //total balance after the transaction. i.e pre-balance: $500.00, transfer $100.00, balance = $400.00
+    settledBalance?: number,
+    moneyAccount?: Account,
 }
